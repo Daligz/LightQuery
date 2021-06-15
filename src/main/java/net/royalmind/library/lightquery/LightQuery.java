8f4b8abf811e5dd0daf6847def8679e400758067
Dali <oscardali370@gmail.com>
2021-06-14 21:52:44 -0500

    Logger removed

--- src/main/java/net/royalmind/library/lightquery/LightQuery.java
+++ src/main/java/net/royalmind/library/lightquery/LightQuery.java
@@ -1,8 +1,3 @@
 package net.royalmind.library.lightquery;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class LightQuery {
-    public static final Logger LOGGER = LoggerFactory.getLogger(LightQuery.class);
-}
+public class LightQuery { }
